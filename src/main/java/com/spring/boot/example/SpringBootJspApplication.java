645330efa6f7598c483b395d480ca11e18c941b7
bangae1 <bangae2@gmail.com>
2016-01-06 23:44:59 +0900

    commit

--- src/main/java/com/spring/boot/example/SpringBootJspApplication.java
+++ src/main/java/com/spring/boot/example/SpringBootJspApplication.java
@@ -2,7 +2,6 @@ package com.spring.boot.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class SpringBootJspApplication {
@@ -10,4 +9,5 @@ public class SpringBootJspApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJspApplication.class, args);
 	}
+
 }
--- src/main/java/com/spring/boot/example/main/controller/MainController.java
+++ src/main/java/com/spring/boot/example/main/controller/MainController.java
@@ -1,18 +1,35 @@
 package com.spring.boot.example.main.controller;
 
+import com.spring.boot.example.main.model.Table;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
 
 /**
  * Created by bangae1 on 2016-01-06.
  */
 @Controller
 public class MainController {
+
+    private static final Logger log = LogManager.getLogger(MainController.class.getName());
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @RequestMapping(value="/", method= RequestMethod.GET)
     public String main(Model model) {
+        List list = jdbcTemplate.query("select * from tab", new BeanPropertyRowMapper(Table.class));
+        System.out.println(list.size());
         model.addAttribute("msg", "hello");
         return "main";
     }
+
 }
